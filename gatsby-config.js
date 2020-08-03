e8e90cfe858b235dfe1d2b7a943e1cb5d09e5541
Josh Levinger <josh@levinger.net>
2020-08-03 11:59:25 -0700

    customize starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Crush2020`,
+    description: ``,
+    author: `@jlev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
