992a47660fc421af2780a91ae15d306ba44ad199
Josh Levinger <josh@levinger.net>
2020-08-04 14:40:08 -0700

    add usa-map for community step

---
+++ src/components/form/steps/community.js
@@ -0,0 +1,22 @@
+import React from "react";
+import USAMap from "react-usa-map"
+
+import { Form, Fieldset, RangeInput } from "@trussworks/react-uswds"
+import { useFormState } from "../context"
+
+export function CommunityStep() {
+  const {
+    state: { time },
+    dispatch
+  } = useFormState();
+
+  return (
+    <Form>
+      <Fieldset legend={"Extend your impact"}>
+        <h2>Adopt a battleground district</h2>
+        <h3>We need to keep the House and flip the Senate. Help win one of these races!</h3>
+        <USAMap />
+      </Fieldset>
+    </Form>
+  );
+}
