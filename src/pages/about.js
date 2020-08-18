4f36c84142b7a92f273ab7cc50fe74215bf769a4
Josh Levinger <josh@levinger.net>
2020-08-18 14:43:06 -0700

    about page update

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,25 +3,43 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { GridContainer } from "@trussworks/react-uswds"
+import { GridContainer, Grid } from "@trussworks/react-uswds"
+
+import josh from "../images/josh.jpg"
+import marisa from "../images/marisa.jpg"
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About Crush2020" />
     <GridContainer>
       <h1>Our Team</h1>
-      <ul>
-        <li>
-            <h2>Marisa Kabas</h2>
-            <p>is Editorial Director of Crush2020. She was previously a staff writer at Fusion and the Daily Dot, and continues to publish freelance writing about feminism, activism, and politics.</p>
-            <p>She is a graduate of George Washington University&apos;s School of Media and Public Affairs and lives in Brooklyn. Find her on Twitter <a href="https://twitter.com/marisakabas">@marisakabas</a>.</p>
-        </li>
-        <li>
-          <h2>Josh Levinger</h2>
-          <p>is the Commander of <a href="https://spacedog.xyz">Spacedog XYZ</a>, a multi-dimensional digital agency for organizations on a mission. He has worked for twelve years at the intersection of technology and activism with groups like CREDO Action, the Electronic Frontier Foundation, Fight for the Future, the Libyan High National Elections Commission, and VoteAmerica. He specializes in designing and building online organizing tools with offline impact.</p>
-          <p>He is a graduate of the Massachusetts Institute of Technology and MIT Media Lab, and lives in Truckee CA. Find him on Twitter <a href="https://twitter.com/jlev">@jlev</a>.</p>
-        </li>
-      </ul>
+      <Grid gap className="usa-graphic-list__row">
+        <Grid tablet={{ col: true }} className="usa-media-block">
+          <img className="usa-media-block__img background-circle" src={marisa} alt="Marisa Kabas"/>
+          <div className="usa-media-block__body">
+            <h2 className="usa-graphic-list__heading">
+              Marisa Kabas, Editorial Director 
+            </h2>
+            <p>
+              Marisa was previously a staff writer at Fusion and the Daily Dot, and continues to publish freelance writing about feminism, activism, and politics.
+              She is a graduate of George Washington University&apos;s School of Media and Public Affairs and lives in Brooklyn.
+              Find her on Twitter <a href="https://twitter.com/marisakabas">@marisakabas</a>.
+            </p>
+          </div>
+        </Grid>
+        <Grid tablet={{ col: true }} className="usa-media-block">
+          <img className="usa-media-block__img background-circle" src={josh} alt="Josh Levinger"/>
+          <div className="usa-media-block__body">
+            <h2 className="usa-graphic-list__heading">
+              Josh Levinger, Technology Director
+            </h2>
+            <p>Josh is the Commander of <a href="https://spacedog.xyz">Spacedog XYZ</a>. He has worked for twelve years at the intersection of technology and activism and specializes in building online organizing tools with offline impact.
+              He is a graduate of the Massachusetts Institute of Technology and MIT Media Lab and lives in Truckee CA.
+              Find him on Twitter <a href="https://twitter.com/jlev">@jlev</a>.
+            </p>
+          </div>
+        </Grid>
+      </Grid>
     </GridContainer>
   </Layout>
 )
