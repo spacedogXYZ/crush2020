5e1645a6368b5cdfc917972c50919d136402271a
Josh Levinger <josh@levinger.net>
2020-09-14 19:45:22 -0700

    mobilize explicitly skip test campaigns
    marked by id

--- data/scripts/get_mobilize.py
+++ data/scripts/get_mobilize.py
@@ -6,6 +6,7 @@ MOBILIZE_URL = "https://www.mobilize.us/api/v1/organizations"
 REQUEST_HEADERS = {'User-Agent' : "bot"}
 DESIRED_FIELDS = ['id', 'name', 'slug', 'race_type', 'org_type', 'state', 'district', 'candidate_name', 'event_feed_url']
 
+TEST_IDS = [883, 1151, 1278, 3298, 3743, 3751, 3755, 4216, 4230, 4539, 4825]
 
 def get_page(url):
     request = urllib.request.Request(url, headers=REQUEST_HEADERS)
@@ -14,7 +15,7 @@ def get_page(url):
 
 def write_page(response, writer):
     for row in response['data']:
-        if row['is_primary_campaign'] is True:
+        if row['is_primary_campaign'] is True or row['id'] in TEST_IDS:
             continue
         out_row = {k: v for k, v in row.items() if k in DESIRED_FIELDS}
         writer.writerow(out_row)
